8f71732a3376ec62eaa46901de5df7fd369bc3be
luanhsd <luan_h.s.d@hotmail.com>
2020-04-07 15:18:15 -0300

    add physicianController

--- 2_Médico/Sprint1/backend/src/routes.js
+++ 2_Médico/Sprint1/backend/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
+//Controllers
+const PhysicianControler = require('./controllers/PhysicianController')
+
 const routes = express.Router()
 
 
-//routes.get('/', controller@index)
+routes.get('/', PhysicianControler.index)
 
 
 module.exports = routes
