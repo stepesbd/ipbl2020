cd5e951d155565ba80e21c3fd9685ab0c7d81abc
Vinicius Lippi <viniplippi@gmail.com>
2020-04-12 19:58:17 -0300

    criacao de routas e edicao bd

--- 2_Médico/Sprint1/backend/src/controllers/PhysicianController.js
+++ 2_Médico/Sprint1/backend/src/controllers/PhysicianController.js
@@ -3,5 +3,17 @@ const connection = require('../database')
 module.exports = {
     async index(request, response) {
         return response.json({msg: 'Hello World !'});
+    },
+    async show(request, response){
+        return response.json({msg: 'Hello World !'}); 
+    },
+    async store(request, response) {
+        return response.json({msg: 'Hello World !'});
+    },
+    async update(request, response){
+        return response.json({msg: 'Hello World !'});
+    },
+    async destroy(request,response){
+        return response.json({msg: 'Hello World !'});
     }
 }
--- 2_Médico/Sprint1/backend/src/routes.js
+++ 2_Médico/Sprint1/backend/src/routes.js
@@ -1,12 +1,16 @@
-const express = require('express')
+const express = require('express');
+const routes = express.Router();
 
 //Controllers
-const PhysicianControler = require('./controllers/PhysicianController')
+const PhysicianControler = require('./controllers/PhysicianController');
 
 const routes = express.Router()
 
+    routes.get("/physicians", PhysicianControler.index)
+    routes.get("/physicians/:id", PhysicianControler.show)
+    routes.post("/physicians", PhysicianControler.store)
+    routes.put("/physicians/:id", PhysicianControler.update)
+    routes.delete("/physicians/:id", PhysicianControler.destroy)
 
-routes.get('/', PhysicianControler.index)
 
-
-module.exports = routes
+module.exports = routes;
