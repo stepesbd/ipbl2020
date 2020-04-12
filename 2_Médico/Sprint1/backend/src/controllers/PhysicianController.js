85e63a269fce98a8acdfca1e5a1e5945c4e2308d
luanhsd <luan_h.s.d@hotmail.com>
2020-04-12 17:48:33 -0300

    add base webapp

--- 2_Médico/Sprint1/backend/src/controllers/PhysicianController.js
+++ 2_Médico/Sprint1/backend/src/controllers/PhysicianController.js
@@ -1,5 +1,8 @@
+const connection = require('../database')
+
 module.exports = {
     async index(request, response) {
+        const physicians = await connection('physicians')
         return response.json('Hello World !')
     }
 }
---
+++ 2_Médico/Sprint1/stepesbd/src/views/Panel/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card, Button } from '@material-ui/core'
+
+export default function Panel() {
+    return (
+        <Card>
+            <Button>Panel</Button>
+        </Card>
+    );
+}
