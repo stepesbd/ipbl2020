009c267ece488fa35c24d2d74ae2f053836730a3
juancanuto <juanhassam2014@gmail.com>
2020-04-15 20:25:41 -0300

    juancanuto
    Alteração do models.py e criação dos testes de fornecedor

--- 4_Fornecedor/Sprint1/Crud/fornecedor/models.py
+++ 4_Fornecedor/Sprint1/Crud/fornecedor/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class Fornecedor(models.Model):
-    cnpj = models.CharField(max_length=200)
+    cnpj = models.IntegerField(validators=[MaxValueValidator(99999999999)])
     razao_social = models.CharField(max_length=200)
     endereco = models.CharField(max_length=200)
-    contato = models.CharField(max_length=200)
+    contato = models.IntegerField(validators=[MaxValueValidator(99999999999)])
 
     # Função responsavel por pela label do objeto numa lista
     def __str__(self):
         return self.razao_social
 
 class Representante(models.Model):
-    cpf = models.CharField(max_length=200)
+    cpf = models.IntegerField(validators=[MaxValueValidator(99999999999)])
     nome = models.CharField(max_length=200)
-    contato = models.CharField(max_length=200)
+    contato = models.IntegerField(validators=[MaxValueValidator(99999999999)])
     fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -28,7 +29,7 @@ class Produto(models.Model):
 
 class Estoque(models.Model):
     descricao = models.CharField(max_length=200)
-    quantidade = models.CharField(max_length=200)
+    quantidade = models.IntegerField(validators=[MaxValueValidator(999999)])
 
     def __str__(self):
         return self.razao_social
--- 4_Fornecedor/Sprint1/Crud/fornecedor/tests.py
+++ 4_Fornecedor/Sprint1/Crud/fornecedor/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from fornecedor.models import Fornecedor
+from django.test import Client
 
-# Create your tests here.
+class FornecedorModelTest(TestCase):
+    # setup dos dados de teste para classe Fornecedor
+    @classmethod
+    def setUpTestData(self):
+        self.fornecedor = Fornecedor.objects.create(cnpj=10000010108, razao_social='Hospital',endereco='av',contato=975555221)
+
+    # teste de nome de atributo
+    def test_cnpj_label(self):
+        fornecedor = Fornecedor.objects.get(id=1)
+        field_label = fornecedor._meta.get_field('cnpj').verbose_name
+        self.assertEquals(field_label, 'cnpj')
+
+    def test_razao_social_label(self):
+        fornecedor = Fornecedor.objects.get(id=1)
+        field_label = fornecedor._meta.get_field('razao_social').verbose_name
+        self.assertEquals(field_label, 'razao social')
+
+    def test_endereco_label(self):
+        fornecedor = Fornecedor.objects.get(id=1)
+        field_label = fornecedor._meta.get_field('endereco').verbose_name
+        self.assertEquals(field_label, 'endereco')
+
+    def test_contato_label(self):
+        fornecedor = Fornecedor.objects.get(id=1)
+        field_label = fornecedor._meta.get_field('contato').verbose_name
+        self.assertEquals(field_label, 'contato')
+
+    # Teste de rota
+    def test_get(self):
+        c = Client()
+        response = c.get('')
+        self.assertEqual(response.status_code, 200)
+
+    # Teste de valores
+    def test_cnpj(self):
+        obj = Fornecedor.objects.get(id=1)
+        field_value = obj.cnpj
+        self.assertEquals(len(str(field_value)), 11)
+
+    def test_contato(self):
+        obj = Fornecedor.objects.get(id=1)
+        field_value = obj.contato
+        self.assertEquals(len(str(field_value)), 9)
+
+    def test_razao_social(self):
+        obj = Fornecedor.objects.get(id=1)
+        field_value = obj.razao_social
+        self.assertEquals(field_value, 'Hospital')
+
+    def test_endereco(self):
+        obj = Fornecedor.objects.get(id=1)
+        field_value = obj.endereco
+        self.assertEquals(field_value, 'av')
+
