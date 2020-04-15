257f5b5888462b3db4b1cb634aad5ec496ecddb1
luanhsd <luan_h.s.d@hotmail.com>
2020-04-15 08:25:54 -0300

    alter routes's file

--- 2_Médico/Sprint1/backend/src/routes/index.js
+++ 2_Médico/Sprint1/backend/src/routes/index.js
@@ -4,14 +4,23 @@ require('express-group-routes');
 
 //Controllers
 const PhysicianControler = require('../controllers/PhysicianController');
+const AddressController = require('../controllers/AddressController');
 
 routes.group("/api", (router) => {
 
+    //Physicians's routes
     router.get("/physicians", PhysicianControler.index)
     router.get("/physicians/:id", PhysicianControler.show)
     router.post("/physicians", PhysicianControler.store)
     router.put("/physicians/:id", PhysicianControler.update)
     router.delete("/physicians/:id", PhysicianControler.destroy)
+
+    //Addresses's routes
+    router.get("/addresses", AddressController.index)
+    router.get("/addresses/:id", AddressController.show)
+    router.post("/addresses", AddressController.store)
+    router.put("/addresses/:id", AddressController.update)
+    router.delete("/addresses/:id", AddressController.destroy)
 
 });
 
