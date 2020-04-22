24f30a31214bd403584b0aaa34ca2ddad8a7ca1c
Lucas Barioni Toma <lbtoma@gmail.com>
2020-04-22 04:33:49 -0300

    [FIX] Fix migrations foreign keys

--- 2_Médico/Sprint1/backend/src/database/migrations/20200410210541_create_contacts.js
+++ 2_Médico/Sprint1/backend/src/database/migrations/20200410210541_create_contacts.js
@@ -2,10 +2,14 @@
 exports.up = function (knex) {
     return knex.schema.createTable('contacts', function (table) {
         table.increments('id').primary()
-        table.integer('physicianId').notNullable()
+        table.integer('physicianId')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('physicians')
+            .onDelete('CASCADE');
         table.string('type').notNullable()
         table.string('contact').notNullable()
-        table.foreign('physicianId').references('id').inTable('physicians')
     })
 };
 
