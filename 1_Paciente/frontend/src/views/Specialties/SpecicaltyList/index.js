488c30dd03d5a401f49a9e0ec34fe4745cd9b3f3
luanhsd <luan_h.s.d@hotmail.com>
2020-05-22 08:14:52 -0300

    add physicians

---
+++ 1_Paciente/frontend/src/views/Specialties/SpecicaltyList/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+// import { Container } from './styles';
+
+function SpecicaltyList() {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default SpecicaltyList;
