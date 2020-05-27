bfe3d9d36c25de13630ffcab99b3a1779f43ec38
Guilherme Manfrim Siviero <guilherme_manfrim@outlook.com>
2020-05-27 00:08:37 -0300

    ajustes no script mqtt 2 mongodb

--- 1_Paciente/Sprint2/rabbitmq2MongoScript/testeSend2Mqtt.py
+++ 1_Paciente/Sprint2/rabbitmq2MongoScript/testeSend2Mqtt.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python
 import pika
+import time
+from threading import Thread
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='mqtt2mongo')
-#Insert
-channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{ "operation": "insert", "name": "test" }')
+channel.queue_declare(queue='mqtt2mongo_ack')
 
+def callback(ch, method, properties, body): 
+    print(body)
+
+def myfunc():
+    channel.basic_consume(queue='mqtt2mongo_ack', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+
+t = Thread(target=myfunc)
+t.start()
+
+input("Press Enter to continue...")
+
+#Insert
+print('Inserting...')
+channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{"operation_id": 1, "operation": "insert", "name": "test" }')
 input("Press Enter to continue...")
 
 #Get
-channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{ "operation": "get" }')
-
+print('Geting...')
+channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{"operation_id": 1, "operation": "get" }')
 input("Press Enter to continue...")
 
 #Update
-channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{ "operation": "update", "attribute": "name", "value": "test", "new_value": "test2" }')
-
+print('Updating...')
+channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{"operation_id": 1, "operation": "update", "attribute": "name", "value": "test", "new_value": "test2" }')
 input("Press Enter to continue...")
 
 #Remove
-channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{ "operation": "remove", "attribute": "name", "value": "test2" }')
+print('removing...')
+channel.basic_publish(exchange='', routing_key='mqtt2mongo', body='{"operation_id": 1, "operation": "remove", "attribute": "name", "value": "test2" }')
+input("Press Enter to continue...")
+
+t.join
 
 connection.close()
