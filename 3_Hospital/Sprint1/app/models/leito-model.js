17fe79f61a5337c65d89191fb4839d348fc29885
Oswaldo Neto <oswaldocstaneto@hotmail.com>
2020-06-04 17:03:54 -0300

    adicionando api hosp-list

--- 3_Hospital/Sprint1/app/models/leito-model.js
+++ 3_Hospital/Sprint1/app/models/leito-model.js
@@ -35,7 +35,8 @@ module.exports = (sequelize, DataTypes) => {
         },
         hos_id:{
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            foreignKey: true
         },
         sector_id:{
             type: DataTypes.INTEGER,
@@ -48,14 +49,14 @@ module.exports = (sequelize, DataTypes) => {
 	});
 
     Bed.associate = function(models){
-        Bed.belongsToMany(models.Hospital, 
-        {
-            through: 'Hospital',
-            foreignKey: 'hos_id', 
-            onDelete: 'CASCADE', 
-            hooks: true,
-            otherKey: 'hos_id',
-        });
+        //Bed.belongsToMany(models.Hospital, 
+        //{
+        //    through: 'Hospital',
+        //    foreignKey: 'hos_id', 
+        //    onDelete: 'CASCADE', 
+        //    hooks: true,
+        //    otherKey: 'hos_id',
+        //});
 
         Bed.hasMany(models.Hospital,{
             foreignKey: 'hos_id',
