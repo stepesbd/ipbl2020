96f598ee727c8eb86f064e82c9db9402c906ebbe
luanhsd <luan_h.s.d@hotmail.com>
2020-06-10 21:07:28 -0300

    add dashboard

--- 2_Médico/Sprint3/frontend/src/components/blog/UsersOverview.js
+++ 2_Médico/Sprint3/frontend/src/components/blog/UsersOverview.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Row, Col, Card, CardHeader, CardBody, Button } from "shards-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Card, CardHeader, CardBody, Button } from 'shards-react';
 
-import RangeDatePicker from "../common/RangeDatePicker";
-import Chart from "../../utils/chart";
+import RangeDatePicker from '../common/RangeDatePicker';
+import Chart from '../../utils/chart';
 
 class UsersOverview extends React.Component {
   constructor(props) {
@@ -17,16 +17,16 @@ class UsersOverview extends React.Component {
       ...{
         responsive: true,
         legend: {
-          position: "top"
+          position: 'top',
         },
         elements: {
           line: {
             // A higher value makes the line look skewed at this ratio.
-            tension: 0.3
+            tension: 0.3,
           },
           point: {
-            radius: 0
-          }
+            radius: 0,
+          },
         },
         scales: {
           xAxes: [
@@ -35,10 +35,10 @@ class UsersOverview extends React.Component {
               ticks: {
                 callback(tick, index) {
                   // Jump every 7 values on the X axis labels to avoid clutter.
-                  return index % 7 !== 0 ? "" : tick;
-                }
-              }
-            }
+                  return index % 7 !== 0 ? '' : tick;
+                },
+              },
+            },
           ],
           yAxes: [
             {
@@ -50,28 +50,28 @@ class UsersOverview extends React.Component {
                   }
                   // Format the amounts using Ks for thousands.
                   return tick > 999 ? `${(tick / 1000).toFixed(1)}K` : tick;
-                }
-              }
-            }
-          ]
+                },
+              },
+            },
+          ],
         },
         hover: {
-          mode: "nearest",
-          intersect: false
+          mode: 'nearest',
+          intersect: false,
         },
         tooltips: {
           custom: false,
-          mode: "nearest",
-          intersect: false
-        }
+          mode: 'nearest',
+          intersect: false,
+        },
       },
-      ...this.props.chartOptions
+      ...this.props.chartOptions,
     };
 
     const BlogUsersOverview = new Chart(this.canvasRef.current, {
-      type: "LineWithLine",
+      type: 'LineWithLine',
       data: this.props.chartData,
-      options: chartOptions
+      options: chartOptions,
     });
 
     // They can still be triggered on hover.
@@ -98,18 +98,20 @@ class UsersOverview extends React.Component {
               <RangeDatePicker />
             </Col>
             <Col>
+              {/*}
               <Button
                 size="sm"
                 className="d-flex btn-white ml-auto mr-auto ml-sm-auto mr-sm-0 mt-3 mt-sm-0"
               >
-                View Full Report &rarr;
+                Relatório Completo &rarr;
               </Button>
+    {*/}
             </Col>
           </Row>
           <canvas
             height="120"
             ref={this.canvasRef}
-            style={{ maxWidth: "100% !important" }}
+            style={{ maxWidth: '100% !important' }}
           />
         </CardBody>
       </Card>
@@ -129,17 +131,17 @@ UsersOverview.propTypes = {
   /**
    * The Chart.js options.
    */
-  chartOptions: PropTypes.object
+  chartOptions: PropTypes.object,
 };
 
 UsersOverview.defaultProps = {
-  title: "Registros Adicionados",
+  title: 'Número De Casos',
   chartData: {
     labels: Array.from(new Array(30), (_, i) => (i === 0 ? 1 : i)),
     datasets: [
       {
-        label: "Current Month",
-        fill: "start",
+        label: '',
+        fill: 'start',
         data: [
           500,
           800,
@@ -170,63 +172,18 @@ UsersOverview.defaultProps = {
           3400,
           2910,
           3100,
-          4250
+          4250,
         ],
-        backgroundColor: "rgba(0,123,255,0.1)",
-        borderColor: "rgba(0,123,255,1)",
-        pointBackgroundColor: "#ffffff",
-        pointHoverBackgroundColor: "rgb(0,123,255)",
+        backgroundColor: 'rgba(0,123,255,0.1)',
+        borderColor: 'rgba(0,123,255,1)',
+        pointBackgroundColor: '#ffffff',
+        pointHoverBackgroundColor: 'rgb(0,123,255)',
         borderWidth: 1.5,
         pointRadius: 0,
-        pointHoverRadius: 3
+        pointHoverRadius: 3,
       },
-      {
-        label: "Past Month",
-        fill: "start",
-        data: [
-          380,
-          430,
-          120,
-          230,
-          410,
-          740,
-          472,
-          219,
-          391,
-          229,
-          400,
-          203,
-          301,
-          380,
-          291,
-          620,
-          700,
-          300,
-          630,
-          402,
-          320,
-          380,
-          289,
-          410,
-          300,
-          530,
-          630,
-          720,
-          780,
-          1200
-        ],
-        backgroundColor: "rgba(255,65,105,0.1)",
-        borderColor: "rgba(255,65,105,1)",
-        pointBackgroundColor: "#ffffff",
-        pointHoverBackgroundColor: "rgba(255,65,105,1)",
-        borderDash: [3, 3],
-        borderWidth: 1,
-        pointRadius: 0,
-        pointHoverRadius: 2,
-        pointBorderColor: "rgba(255,65,105,1)"
-      }
-    ]
-  }
+    ],
+  },
 };
 
 export default UsersOverview;
--- 2_Médico/Sprint3/frontend/src/views/Dashboard.js
+++ 2_Médico/Sprint3/frontend/src/views/Dashboard.js
@@ -1,20 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Container, Row, Col } from "shards-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'shards-react';
 
-import PageTitle from "./../components/common/PageTitle";
-import SmallStats from "./../components/common/SmallStats";
-import UsersOverview from "./../components/blog/UsersOverview";
-import UsersByDevice from "./../components/blog/UsersByDevice";
-import NewDraft from "./../components/blog/NewDraft";
-import Discussions from "./../components/blog/Discussions";
-import TopReferrals from "./../components/common/TopReferrals";
+import PageTitle from './../components/common/PageTitle';
+import SmallStats from './../components/common/SmallStats';
+import UsersOverview from './../components/blog/UsersOverview';
+import UsersByDevice from './../components/blog/UsersByDevice';
 
 const Dashboard = ({ smallStats }) => (
   <Container fluid className="main-content-container px-4">
     {/* Page Header */}
     <Row noGutters className="page-header py-4">
-      <PageTitle title="Indicadores" subtitle="Dashboard" className="text-sm-left mb-3" />
+      <PageTitle
+        title="Indicadores"
+        subtitle="Dashboard"
+        className="text-sm-left mb-3"
+      />
     </Row>
 
     {/* Small Stats Blocks */}
@@ -37,25 +38,12 @@ const Dashboard = ({ smallStats }) => (
     </Row>
 
     <Row>
-
       <Col lg="8" md="12" sm="12" className="mb-4">
         <UsersOverview />
       </Col>
 
       <Col lg="4" md="6" sm="12" className="mb-4">
         <UsersByDevice />
-      </Col>
-
-      <Col lg="4" md="6" sm="12" className="mb-4">
-        <NewDraft />
-      </Col>
-
-      <Col lg="5" md="12" sm="12" className="mb-4">
-        <Discussions />
-      </Col>
-
-      <Col lg="3" md="12" sm="12" className="mb-4">
-        <TopReferrals />
       </Col>
     </Row>
   </Container>
@@ -65,105 +53,105 @@ Dashboard.propTypes = {
   /**
    * The small stats dataset.
    */
-  smallStats: PropTypes.array
+  smallStats: PropTypes.array,
 };
 
 Dashboard.defaultProps = {
   smallStats: [
     {
-      label: "Indicador 1",
-      value: "2,390",
-      percentage: "4.7%",
+      label: 'Total de Pacientes',
+      value: '2,390',
+      percentage: '4.7%',
       increase: true,
       chartLabels: [null, null, null, null, null, null, null],
-      attrs: { md: "6", sm: "6" },
+      attrs: { md: '6', sm: '6' },
       datasets: [
         {
-          label: "Today",
-          fill: "start",
+          label: 'Today',
+          fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: "rgba(0, 184, 216, 0.1)",
-          borderColor: "rgb(0, 184, 216)",
-          data: [1, 2, 1, 3, 5, 4, 7]
-        }
-      ]
+          backgroundColor: 'rgba(0, 184, 216, 0.1)',
+          borderColor: 'rgb(0, 184, 216)',
+          data: [1, 2, 1, 3, 5, 4, 7],
+        },
+      ],
     },
     {
-      label: "Indicador 2",
-      value: "182",
-      percentage: "12.4",
+      label: 'Recuperados',
+      value: '182',
+      percentage: '12.4',
       increase: true,
       chartLabels: [null, null, null, null, null, null, null],
-      attrs: { md: "6", sm: "6" },
+      attrs: { md: '6', sm: '6' },
       datasets: [
         {
-          label: "Today",
-          fill: "start",
+          label: 'Today',
+          fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: "rgba(23,198,113,0.1)",
-          borderColor: "rgb(23,198,113)",
-          data: [1, 2, 3, 3, 3, 4, 4]
-        }
-      ]
+          backgroundColor: 'rgba(23,198,113,0.1)',
+          borderColor: 'rgb(23,198,113)',
+          data: [1, 2, 3, 3, 3, 4, 4],
+        },
+      ],
     },
     {
-      label: "Indicador 3",
-      value: "8,147",
-      percentage: "3.8%",
+      label: 'Testado Positivo',
+      value: '8,147',
+      percentage: '3.8%',
       increase: false,
       decrease: true,
       chartLabels: [null, null, null, null, null, null, null],
-      attrs: { md: "4", sm: "6" },
+      attrs: { md: '4', sm: '6' },
       datasets: [
         {
-          label: "Today",
-          fill: "start",
+          label: 'Today',
+          fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: "rgba(255,180,0,0.1)",
-          borderColor: "rgb(255,180,0)",
-          data: [2, 3, 3, 3, 4, 3, 3]
-        }
-      ]
+          backgroundColor: 'rgba(255,180,0,0.1)',
+          borderColor: 'rgb(255,180,0)',
+          data: [2, 3, 3, 3, 4, 3, 3],
+        },
+      ],
     },
     {
-      label: "Indicador 4",
-      value: "29",
-      percentage: "2.71%",
+      label: 'Óbitos',
+      value: '29',
+      percentage: '2.71%',
       increase: false,
       decrease: true,
       chartLabels: [null, null, null, null, null, null, null],
-      attrs: { md: "4", sm: "6" },
+      attrs: { md: '4', sm: '6' },
       datasets: [
         {
-          label: "Today",
-          fill: "start",
+          label: 'Today',
+          fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: "rgba(255,65,105,0.1)",
-          borderColor: "rgb(255,65,105)",
-          data: [1, 7, 1, 3, 1, 4, 8]
-        }
-      ]
+          backgroundColor: 'rgba(255,65,105,0.1)',
+          borderColor: 'rgb(255,65,105)',
+          data: [1, 7, 1, 3, 1, 4, 8],
+        },
+      ],
     },
     {
-      label: "Indicador 5",
-      value: "17,281",
-      percentage: "2.4%",
+      label: 'Pacientes com Sintomas',
+      value: '17,281',
+      percentage: '2.4%',
       increase: false,
       decrease: true,
       chartLabels: [null, null, null, null, null, null, null],
-      attrs: { md: "4", sm: "6" },
+      attrs: { md: '4', sm: '6' },
       datasets: [
         {
-          label: "Today",
-          fill: "start",
+          label: 'Today',
+          fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: "rgb(0,123,255,0.1)",
-          borderColor: "rgb(0,123,255)",
-          data: [3, 2, 3, 2, 4, 5, 4]
-        }
-      ]
-    }
-  ]
+          backgroundColor: 'rgb(0,123,255,0.1)',
+          borderColor: 'rgb(0,123,255)',
+          data: [3, 2, 3, 2, 4, 5, 4],
+        },
+      ],
+    },
+  ],
 };
 
 export default Dashboard;
