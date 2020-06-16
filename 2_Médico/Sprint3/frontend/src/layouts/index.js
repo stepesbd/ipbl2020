6409c1284e72876a3a9bbd8e1d4d7dff6c3e7bc8
luanhsd <luan_h.s.d@hotmail.com>
2020-06-16 11:50:02 -0300

    drop views

--- 2_Médico/Sprint3/frontend/src/layouts/index.js
+++
@@ -1,5 +0,0 @@
-import DefaultLayout from './Default';
-import Default2Layout from './Default2';
-import LoginLayout from './LoginLayout';
-
-export { DefaultLayout, Default2Layout, LoginLayout };
--- 2_Médico/Sprint3/frontend/src/utils/history.js
+++
@@ -1,5 +0,0 @@
-
-import createHistory from 'history/createBrowserHistory'
-const history = createHistory()
-history.pushLater = (...args) => setImmediate(() => history.push(...args))
-export default history
