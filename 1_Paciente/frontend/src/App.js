c2687ca02b724986b8e35c32bdd744fa828f1112
luanhsd <luan_h.s.d@hotmail.com>
2020-06-29 08:41:50 -0300

    alter specialtie's views

--- 1_Paciente/frontend/src/App.js
+++ 1_Paciente/frontend/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import history from "./utils/history";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import history from './utils/history';
 
-import routes from "./routes";
+import routes from './routes';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/styles/shards-dashboards.1.1.0.min.css";
-import "./assets/styles/other.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/shards-dashboards.1.1.0.min.css';
+import './assets/styles/other.css';
 
 export default () => (
-  <Router basename={process.env.REACT_APP_BASENAME || ""} >
+  <Router basename={process.env.REACT_APP_BASENAME || ''}>
     <div>
       {routes.map((route, index) => {
         return (
@@ -18,13 +18,13 @@ export default () => (
             path={route.path}
             exact={route.exact}
             history={history}
-            component={(props => {
+            component={(props) => {
               return (
                 <route.layout {...props}>
                   <route.component {...props} />
                 </route.layout>
               );
-            })}
+            }}
           />
         );
       })}
